c342d3c7a48b1c08177e03cdad4f34ecbb430c84
Paule <popomartel@live.com>
2020-06-09 14:03:48 -0400

    ajout d'affaire très de base pour commencer le TP

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(int argv, char * argv[]){
+    bool finProg = false;
+
+    while(!finProg){
+        //lire et valider le fichier
+
+        //demande de requetes a l'utilisateur
+        
+
+    }
+
+}
