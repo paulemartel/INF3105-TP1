6fecaad830cfcef201448570bca717e3cc3935de
Gaby <poitras.gabrielle.2@courriel.uqam.ca>
2020-06-13 12:06:18 -0400

    2 premieres validations faites

--- Engrenage.cpp
+++ Engrenage.cpp
@@ -1,7 +1,15 @@
 #include "Engrenage.hpp"
 
+#include <iostream>
+
 Engrenage::Engrenage (int nombreDent) {
-    nombreDentOuSillon = nombreDent;
+    
+    if (nombreDent >= 6) {
+        nombreDentOuSillon = nombreDent;
+    } else {
+        cerr << "Erreur (mettre constante ici)" << endl;
+        exit(-1);
+    }
 }
 
 int Engrenage::getNombreDent() {
--- Vis.cpp
+++ Vis.cpp
@@ -1,7 +1,15 @@
 #include "Vis.hpp"
 
+#include <iostream>
+
 Vis::Vis (int nombreSillon) {
-    nombreDentOuSillon = nombreSillon;
+    
+    if (nombreSillon > 0 && nombreSillon < 5) {
+        nombreDentOuSillon = nombreSillon;
+    } else {
+        cerr << "erreur (mettre constante ici)" << endl;
+        exit(-1);
+    }
 }
 
 int Vis::getNombreSillon() {
